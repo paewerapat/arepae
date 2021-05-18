44640ec4a092f9d109302ced64a63095da1dc853
paewerapat <r.paetv@gmail.com>
2021-05-18 15:59:18 +0700

    Header Complete

---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Web Development and Web Design</h1>
+                <Typed 
+                    className="typed-text"
+                    strings={["Web Developer", "Web Design", "Wordpress", "UX/UI"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main-offer">Contact me</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
